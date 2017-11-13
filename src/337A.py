4629ce38898951e24b6e3f056d670bdc6bc0743c
spandan <spandan@prosperworks.com>
2017-11-13 00:27:23 -0800

    337A.py added

---
+++ src/337A.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+f = list(map(int, input().split()))
+
+f.sort()
+
+best = f[n - 1] - f[0]
+
+for k in range(1, m - n + 1):
+    best = min(best, f[k + n - 1] - f[k])
+
+print(best)
